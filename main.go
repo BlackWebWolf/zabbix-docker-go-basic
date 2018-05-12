187cfab5de596bd6b894c31993098ee80a5cb16a
Wilq <michal.kajszczak@espeo.eu>
2018-05-12 19:28:21 +0200

    working container names

--- main.go
+++ main.go
@@ -7,22 +7,29 @@ import (
 	"docker.io/go-docker/api/types"
 	"fmt"
 	"context"
+	"flag"
+	// "reflect"
 	// "encoding/json"
 	// "time"
 )
-// type zbdata struct {
-//     data   int      `json:"data"`
-// }
-//
-// type container struct {
-// 	name string `json:{#CONTAINER}`
-// }
-const (
-	defaultHost  = `localhost`
-	defaultPort  = 10051
-)
+// const (
+// 	defaultHost  = `localhost`
+// 	defaultPort  = 10051
+// )
 
 func main() {
+
+
+	defaultHost := flag.String("zabbix","localhost","zabbix server e.g. '127.0.0.1'")
+targetHost := flag.String("host","ZBagent","zabbix target host e.g. 'myhost'")
+defaultPort := flag.Int("port",10051,"zabbix server port e.g. 10051")
+flag.Parse()
+
+// make sure required fields 'zabbix' and 'host' are populated
+if *defaultHost=="" || *targetHost=="" {
+	flag.PrintDefaults()
+	return
+}
 	cli, err := docker.NewEnvClient()
 	if err != nil {
 		panic(err)
@@ -33,32 +40,32 @@ func main() {
 		panic(err)
 	}
 			var metrics []*Metric
-	// test := `{"data":[{"{#CONTAINER}":"`+ container.Image+`"}]}`
 	test1 := `{"data":[`
 	for i, container := range containers {
-		fmt.Printf("%s %s\n", container.State, container.Image)
+
+		fmt.Printf("%s %s\n", container.State, container.Names[0])
 
 		var app string
 		if i < len(containers)-1 {
-			app = `{"{#CONTAINER}":"` + container.Image+`"},`
+			app = `{"{#CONTAINER}":"` + container.Names[0]+`"},`
 		} else {
-			app = `{"{#CONTAINER}":"` + container.Image+`"}`
+			app = `{"{#CONTAINER}":"` + container.Names[0]+`"}`
 		}
 		test1 = test1 + app
 
 
 
-		metrics = append(metrics, NewMetric("ZBagent", "docker.["+container.Image+"]", container.State))
+		metrics = append(metrics, NewMetric(*targetHost, "docker.["+container.Names[0]+"]", container.State))
 
 		//// Create instance of Packet class
 
 	}
 	test1 = test1 + `]}`
-			metrics = append(metrics, NewMetric("ZBagent", "docker.discovery", test1))
+			metrics = append(metrics, NewMetric(*targetHost, "docker.discovery", test1))
 
 			packet := NewPacket(metrics)
 			//
 			//// Send packet to zabbix
-			z := NewSender(defaultHost, defaultPort)
+			z := NewSender(*defaultHost, *defaultPort)
 			z.Send(packet)
 }
